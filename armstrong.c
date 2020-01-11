b834d3742fd47b46d58d09d338d785e5b97caa4d
VaishnaviKul <40765352+VaishnaviKul@users.noreply.github.com>
2020-01-11 16:59:17 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,13 +1,13 @@
     #include<stdio.h>  
     int main()    
     {    
-    int num,r,sum=0,temp;    
+    int num,sum=0,temp;    
     printf("enter the number=");    
     scanf("%d",&num);    
     temp=num;    
     while(num>0)    
     {    
-    r=num%10;    
+    int r=num%10;    
     sum=sum+(r*r*r);    
     num=num/10;    
     }    
